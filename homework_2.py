5b55dfcabc1e39fec80f271e2d2bcfc493212a87
hp Egor <egorbache25@gmail.com>
2024-05-08 19:32:23 +0300

    Python work 06.05.24

---
+++ homework_2.py
@@ -0,0 +1,14 @@
+# ДЗ 2.1. Виведення числа в стовпчик
+number = int(input("Enter a 4-digit number: "))
+
+print("Result: ")
+print(number // 1000)
+print((number % 1000) // 100)
+print((number % 100) // 10)
+print(number % 10)
+
+# ДЗ 2.2. Необхідно "перевернути" 5-ти значне число
+number = int(input("Enter a 5-digit positive integer: "))
+
+reversed_number = (number % 10) * 10000 + (number % 100 // 10) * 1000 + (number % 1000 // 100) * 100 +(number // 1000 % 10) * 10 + (number // 10000)
+print("Reversed number: ", reversed_number)
---
+++ lesson_1.py
@@ -0,0 +1,36 @@
+# print("Hello world!")
+# print()
+# print('test')
+
+# print("Hello world", "Text1", "Text2", sep=", ", end=" - ")
+# print("Hello world")
+# print('test')
+
+# однорядковий коментар
+
+'''
+три одинарні лапки
+багаторядковий
+коментар
+тут можна писати будь-який текст і він буде проігнорований інтерпретатором
+'''
+
+# Ctrl + / -> comment или uncomment
+
+# print("hello11")
+# print("hello22")
+#
+# aljfjklsdfjkvskjfd
+# print("qwerty")
+
+####
+# # escape послідовності
+# # \n -> перенесення на новий рядок
+# print("Hello\nworld")
+# # \t -> табуляція -> 4 пробіли. (буває в консолі 2 або 8 пробіли)
+# print("Hello\n\tworld")
+# # \ -> дзеркалювання, екранування – якщо необхідно службовий символ зробити друкованим
+# print("Hello\\n\\t\"world\"")
+# print("\\\\\\\\\\")
+
+print("Hello world")
